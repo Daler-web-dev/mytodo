1a5ec597c9099479684a674a31c0d77ee149aed3
dalerb <example@gmail.com>
2021-01-15 17:57:36 +0500

    second commit

--- src/main.js
+++ src/main.js
@@ -2,3 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+
+let arr = 'hello world'
+
+console.log(arr.slice(6, 11));
+
+let peoples = ['daler', 'ravhskan', 'amir', 'murod']
+
+console.log(peoples.slice(0, 2));
